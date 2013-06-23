87ed15d07b8dbb2364911cbe356e65473a48dbfa
tinasoul <tinapshah@gmail.com>
2013-06-23 17:00:14 -0500

    Inital commit. Added vendor/bundle to.gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boost::Application.config.secret_token = '1433c6df719956e5c9b9d2d1091025c541f505fa3174932b9b0068cd73efa7b701a4d811257bf30b33465bdb8aead2eed099b98b6af0faf703bf559f0d7bd78d'
